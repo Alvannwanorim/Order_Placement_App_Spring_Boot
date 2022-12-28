cd4de3f67c5c5ddf543478c99bb0cc4e41a91684
alvannwanorim <alvannwanorim@gmail.com>
2022-12-29 00:14:25 +0100

    Feat: setup

---
+++ src/test/java/com/alvan/orderingapp/OrderingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alvan.orderingapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderingAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
